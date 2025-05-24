3341fcd56cb46c2126bcda62c68372b304fce025
山田さん <yamadasan@yamadasannoMacBook-Air.local>
2025-05-24 18:43:15 +0900

    コード更新

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pushup-counter/',
+  base: "/push-up-counter_React/",
   plugins: [react()],
 })
